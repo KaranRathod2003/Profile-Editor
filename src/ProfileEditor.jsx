8c45beee6559d9f44f4da24058c3a11bf6dfd9cf
KaranRathod2003 <kr04391@gmail.com>
2025-09-09 12:50:14 +0530

    Current working version without folders

--- src/ProfileEditor.jsx
+++ src/ProfileEditor.jsx
@@ -1,6 +1,10 @@
   import React, { useState } from 'react'
   import ProfileCard from './components/ProfileCard'
   import ProfileForm from './components/ProfileForm'
+  import Alert from '@mui/material/Alert';
+import { Snackbar } from '@mui/material';
+
+
 
   const ProfileEditor = () => {
     const [isEditing, setIsEditing] = useState(false);
@@ -8,6 +12,11 @@
       name: 'Your Name',
       email: 'Your Email',
       bio: 'Your Bio...'
+    })
+    const [alert, setAlert] = useState({
+      open:false,
+      message:"",
+      type:""
     })
     const [backupProfileData, setBackupProfileData] = useState(profileData);
 
@@ -24,29 +33,31 @@
 
 
     // seperately handling each change name
-    const handleName = (e) =>{
-      setProfileData({
-        ...profileData,
-        name:e.target.value
-      })
+    // const handleName = (e) =>{
+    //   setProfileData({
+    //     ...profileData,
+    //     name:e.target.value
+    //   })
+    // }
+    // const handleEmail = (e) =>{
+    //   setProfileData({
+    //     ...profileData,
+    //     email:e.target.value
+    //   })
+    // }
+    // const handleBio = (e) =>{
+    //   setProfileData({
+    //     ...profileData,
+    //     bio:e.target.value
+    //   })
+    // }
+
+
+
+
+    const handleClose = () =>{
+      setAlert({...alert, open:false})
     }
-    const handleEmail = (e) =>{
-      setProfileData({
-        ...profileData,
-        email:e.target.value
-      })
-    }
-    const handleBio = (e) =>{
-      setProfileData({
-        ...profileData,
-        bio:e.target.value
-      })
-    }
-
-
-
-
-
     const handleEdit = () => {
       setBackupProfileData(profileData)
       setIsEditing(true)
@@ -54,14 +65,31 @@
     const handleSave = () => {
       // setProfileData()
       setIsEditing(false)
+      setAlert({ open: true, type: "success", message: "Profile updated successfully!" });
     }
     const handleCancel = () => {
       setProfileData(backupProfileData)
       setIsEditing(false)
+      setAlert({ open: true, type: "info", message: "Edits discarded" });
     }
     return (
       <>
         {isEditing ? <ProfileForm handleChange = {handleChange} name = {profileData.name} email={profileData.email} bio={profileData.bio} handleCancel={handleCancel} handleSave={handleSave} /> : <ProfileCard name={profileData.name} email={profileData.email} bio={profileData.bio} handleEdit={handleEdit} />}
+        <Snackbar 
+          open = {alert.open}
+          autoHideDuration={3000}
+          anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+          onClose={handleClose}
+        >
+          <Alert
+          onClose={handleClose} 
+          severity={alert.type}
+          sx={{ width: '100%' }}
+          >
+            {alert.message}
+
+          </Alert>
+        </Snackbar>
       </>
     )
   }
