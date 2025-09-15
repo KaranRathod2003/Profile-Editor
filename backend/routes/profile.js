5dcab5b699d5b894109a8b1e4e033686dfb989bc
KaranRathod2003 <kr04391@gmail.com>
2025-09-15 14:15:19 +0530

    feat: DELETE profile reset

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -3,9 +3,11 @@ const express = require('express');
 
 let profile = {};
 const router = express.Router();
+
 router.get('/', (req, res)=>{
     res.json(profile);
 })
+
 router.post('/', (req, res, next) => {
     profile = { ...profile, ...req.body };
     console.log("POST /api/profile - Updated Profile", profile);
@@ -16,6 +18,12 @@ router.put('/', (req, res)=>{
     const { name, email, bio } = req.body;
     profile = {...profile, ...req.body};
     console.log("PUT /api/profile", profile);
+    res.json(profile);
+})
+
+router.delete('/', (req, res)=>{
+    profile = {};
+    console.log("Profile deleted successfully !!", profile)
     res.json(profile);
 })
 
